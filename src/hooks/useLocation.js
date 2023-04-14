620da1d9dfe76fba57cd800d67698804fd330a39
bradley2w1dl <bradley@himaxwell.com>
2023-04-14 08:14:52 -0600

    Add useLocation hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+
+// use location hook for simple initial "routing" implementation
+const useLocation = () => {
+  const 
+  return [path, setPath]
+}
