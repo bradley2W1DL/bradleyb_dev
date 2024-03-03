b097bac411c8b9a8b383c31c5fdb04d7eb47d0d7
bradley2w1dl <bradley@himaxwell.com>
2024-03-03 09:38:16 -0700

    💣 fully switch over from React app to Astro

--- src/hooks/useLocation.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react'
-
-// use location hook for simple initial "routing" implementation
-const useLocation = () => {
-  const 
-  return [path, setPath]
-}
