6c4bbdb79cc63fb142a2a7a1fb2ccc515e49b432
bradley2w1dl <bradley@himaxwell.com>
2024-10-04 09:44:33 -0600

    next chunk of boilerplate for weather.getCurrent

--- src/actions/weather.ts
+++ src/actions/weather.ts
@@ -1,11 +1,11 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
-const fetchWeather = async () => {
+const fetchWeather = async ({ lat, long }) => {
   //const response = await fetch(url)
 
   // parse response somewhat??
-  return "this is today's weather"
+  return `this is today's weather at ${lat}:${long}`
 }
 
 // "z" here is Zod (schema strong-typing)
