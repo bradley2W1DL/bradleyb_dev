0c89a58b9e6e7f3dab76339f334fd591cff00727
bradley2w1dl <bradley@himaxwell.com>
2024-10-08 22:28:24 -0600

    WIP implement weather api call

--- src/actions/weather.ts
+++ src/actions/weather.ts
@@ -2,15 +2,21 @@ import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
 const fetchWeather = async ({ lat, long }) => {
-  //const response = await fetch(url)
+  const queryParams = encodeURIComponent(`${lat},${long}`)
+  const url = `https://dev.pirateweather.net/forecast/${apiKey}/${queryParams}`
+  console.log(url)
+  // TODO, this may need to implement some caching...only refresh every 5 or 10 mins
+  const response = await fetch(url, { headers: { 'Accept': 'application/json' }})
 
-  // parse response somewhat??
-  return `this is today's weather at ${lat}:${long}`
+  return await response.json()
 }
+
+const apiKey = import.meta.env.PIRATE_WEATHER_API_KEY
 
 // "z" here is Zod (schema strong-typing)
 export const weather = {
   getCurrent: defineAction({
+    accept: 'json',
     input: z.object({
       lat: z.number(),
       long: z.number(),
